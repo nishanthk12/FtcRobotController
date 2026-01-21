53ff3e4b3d3aa7a83ad3585050102827781b3c28
nishanthk12 <nishanthkarupakula@gmail.com>
2026-01-20 20:32:38 -0600

    auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutoMecanum.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous
+public class TestAutoMecanum extends OpMode {
+
+    private DcMotor FrontLeft, FrontRight, BackLeft, BackRight;
+
+    @Override
+    public void init() {
+
+        FrontLeft = hardwareMap.get(DcMotor.class, "front_left");
+        FrontRight = hardwareMap.get(DcMotor.class, "front_right");
+        BackLeft = hardwareMap.get(DcMotor.class, "back_left");
+        BackRight = hardwareMap.get(DcMotor.class, "back_right");
+
+        FrontRight.setDirection(DcMotor.Direction.REVERSE);
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
+
+        FrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
+    @Override
+    public void start() {
+
+        drive(0.25, 0.0, 0.0);
+        try { Thread.sleep(2000); } catch (Exception e) {}
+        drive(-0.25, 0.0, 0.0);
+        try { Thread.sleep(2000); } catch (Exception e) {}
+        drive(0.0, 0.10, 0.0);
+        try { Thread.sleep(2000); } catch (Exception e) {}
+        drive(0.0, -0.10, 0.0);
+        try { Thread.sleep(2000); } catch (Exception e) {}
+        drive(0.0, 0.0, 0.0);
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+        drive(0.0, 0.0, 0.0);
+    }
+
+    public void drive(double forward, double strafe, double rotate) {
+        double FrontLeftPower = forward - strafe - rotate;
+        double FrontRightPower = forward + strafe + rotate;
+        //double BackLeftPower = -forward - strafe + rotate;
+        double BackLeftPower = forward + strafe - rotate;
+        double BackRightPower = forward - strafe + rotate;
+
+        double maxPower = 0.25;
+        double maxSpeed = 0.25;
+
+        maxPower = Math.max(maxPower, Math.abs(FrontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(FrontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(BackLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(BackRightPower));
+
+        FrontLeft.setPower(maxSpeed * FrontLeftPower / maxPower);
+        FrontRight.setPower(maxSpeed * FrontRightPower / maxPower);
+        BackLeft.setPower(maxSpeed * BackLeftPower / maxPower);
+        BackRight.setPower(maxSpeed * BackRightPower / maxPower);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestGoBildaPinpoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestGoBildaPinpoint.java
@@ -14,7 +14,7 @@ import java.util.Locale;
 
 
 
-
+@Disabled
 @TeleOp
 public class TestGoBildaPinpoint extends LinearOpMode  {
 
